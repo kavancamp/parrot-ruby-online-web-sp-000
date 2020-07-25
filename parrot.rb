b84269455fb9d97f6d3901d1571e4a97eb6ff3c3
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-25 16:02:29 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot( phrase = "Pretty bird!")
+def parrot( phrase = "Squawk!")
 puts phrase
- "Squawk!"
+ "Pretty bird!"
 
 end
